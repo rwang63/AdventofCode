03588e277f82ae1526ad4d19ce144a2b06a649ed
rwang <rwang@seattleu.edu>
2021-12-20 00:39:39 -0800

    Complete calculatePositionWithAim for second half of day 2 problem

--- src/Dive.java
+++ src/Dive.java
@@ -7,8 +7,11 @@ public class Dive {
 
     public static void main(String[] args) {
         int result = calculatePosition(filePath);
+        int resultWithAim = calculatePositionWithAim(filePath);
 
         System.out.println("Multiplying the horizontal position with the final depth gives us: " + result + ".");
+        System.out.println("The result with aim is: " + resultWithAim + ".");
+
     }
 
     public static int calculatePosition(String filePath) {
@@ -32,6 +35,42 @@ public class Dive {
                         break;
                     case "down":
                         depth += amount;
+                        break;
+                }
+            }
+            finalResult = horizontalPosition * depth;
+        } catch (FileNotFoundException e) {
+            System.out.println("Error in reading file");
+            e.printStackTrace();
+        }
+        return finalResult;
+    }
+
+    public static int calculatePositionWithAim(String filePath) {
+        int finalResult = 0;
+        int aim = 0;
+        String direction;
+        int amount;
+        int horizontalPosition = 0;
+        int depth = 0;
+        try {
+            File myObj = new File(filePath);
+            Scanner myReader = new Scanner(myObj);
+            while(myReader.hasNextLine()) {
+                direction = myReader.next();
+                amount = myReader.nextInt();
+                switch(direction) {
+                    case "forward":
+                        horizontalPosition += amount;
+                        if(aim > 0) {
+                            depth += aim * amount;
+                        }
+                        break;
+                    case "up":
+                        aim -= amount;
+                        break;
+                    case "down":
+                        aim += amount;
                         break;
                 }
             }

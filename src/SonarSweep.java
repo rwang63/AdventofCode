e9f0084e5da51254e5064bbf30f43b27cc980ed7
rwang <rwang@seattleu.edu>
2021-12-20 00:30:35 -0800

    Complete tripleSlidingWindow for second half of day 1 problem

--- src/SonarSweep.java
+++ src/SonarSweep.java
@@ -7,6 +7,7 @@ public class SonarSweep {
 
     public static void main(String[] args) {
         countIncrements(filePath);
+        tripleSlidingWindow(filePath);
     }
 
     public static void countIncrements(String filePath) {
@@ -24,6 +25,36 @@ public class SonarSweep {
                 currentDataPoint = nextDataPoint;
             }
             System.out.println("There are " + counter + " instances of increasing in the file.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error in reading file");
+            e.printStackTrace();
+        }
+    }
+
+    public static void tripleSlidingWindow(String filePath) {
+        int counter = 0;
+        int currentTriplet;
+        int nextTriplet;
+        int nextDataPoint;
+        try {
+            File myObj = new File(filePath);
+            Scanner myReader = new Scanner(myObj);
+            int firstOfThree = myReader.nextInt();
+            int secondOfThree = myReader.nextInt();
+            int thirdOfThree = myReader.nextInt();
+            currentTriplet = firstOfThree + secondOfThree + thirdOfThree;
+            while(myReader.hasNextLine()) {
+                nextDataPoint = myReader.nextInt();
+                nextTriplet = currentTriplet - firstOfThree + nextDataPoint;
+                if(nextTriplet > currentTriplet) {
+                    counter++;
+                }
+                firstOfThree = secondOfThree;
+                secondOfThree = thirdOfThree;
+                thirdOfThree = nextDataPoint;
+                currentTriplet = nextTriplet;
+            }
+            System.out.println("There are " + counter + " instances of three-measurement increases in the file.");
         } catch (FileNotFoundException e) {
             System.out.println("Error in reading file");
             e.printStackTrace();
